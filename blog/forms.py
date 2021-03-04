2bc970e44944101c6fc66f533a0a21511db8f00a
anupam <guruanupamr888@gmail.com>
2021-03-04 21:17:02 +0530

    first_commit

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required = False, widget=forms.Textarea)
+
+from .models import comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = comment
+        fields = ('name','email','body')
